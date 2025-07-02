28f7f18e416c9452b5d85926d7c2b4ec93600f36
Chisanucha Thonsiri <chisanucha.tho@dome.tu.ac.th>
2025-07-02 23:27:35 +0700

    ui: scrollable post section

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Menubar(){
     return <div className='Menubar'>
     <div className='Menu Selected'><i class="fa-solid fa-message"></i> โพสต์</div>
+    <div className='Menu Unselected'><i class="fa-solid fa-message"></i> ห้องแชท</div>
     <div className='Menu Unselected'><i class="fa-solid fa-file"> </i> เอกสารการเรียน</div>
     <div className='Menu Unselected'><i class="fa-solid fa-film"> </i> คลิป</div>
     <div className='Menu Unselected'><i class="fa-solid fa-circle-check"> </i> แบบฝึกหัด</div>
